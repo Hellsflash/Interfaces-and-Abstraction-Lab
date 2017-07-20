3fa00e8d6104ca5cefdfdcd7a797a75483409a0c
Mario Slavov <hellsflash@gmail.com>
2017-07-20 03:54:48 +0300

    Add project files.

---
+++ Cars/Seat.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+
+public class Seat : ICar
+{
+    public Seat(string model, string color)
+    {
+        Model = model;
+        Color = color;
+    }
+
+    public string Model { get; set; }
+    public string Color { get; set; }
+
+    public string Start()
+    {
+        return "Engine start";
+    }
+
+    public string Stop()
+    {
+        return "Breaaak!";
+    }
+
+    public override string ToString()
+    {
+        StringBuilder sb = new StringBuilder();
+
+        sb.AppendLine($"{this.Color} {this.GetType().Name} {this.Model}");
+        sb.AppendLine(Start());
+        sb.AppendLine(Stop());
+        return sb.ToString().Trim();
+    }
+}
---
+++ Cars/Tesla.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+
+public class Tesla : ICar, IElectricCar
+{
+    public Tesla(string model, string color, int battery)
+    {
+        Model = model;
+        Color = color;
+        Battery = battery;
+    }
+
+    public string Model { get; set; }
+    public string Color { get; set; }
+    public int Battery { get; set; }
+
+    public string Start()
+    {
+        return "Engine start";
+    }
+
+    public string Stop()
+    {
+        return "Breaaak!";
+    }
+
+    public override string ToString()
+    {
+
+        StringBuilder sb = new StringBuilder();
+
+        sb.AppendLine($"{this.Color} {this.GetType().Name} {this.Model} with {this.Battery} Batteries");
+        sb.AppendLine(Start());
+        sb.AppendLine(Stop());
+        return sb.ToString().Trim();
+    }
+}
